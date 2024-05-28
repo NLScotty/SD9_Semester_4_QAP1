0150bbf6bdeba10c75be33da986fb87b4a5df248
Scott Normore <Scott.B.Normore@gmail.com>
2024-05-28 09:37:27 -0230

    Removed Mockito and changed a unit test so it correctly functions.

--- src/test/java/com/keyin/SuggestionEngineTest.java
+++ src/test/java/com/keyin/SuggestionEngineTest.java
@@ -12,20 +12,13 @@ import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
-@ExtendWith(MockitoExtension.class)
 public class SuggestionEngineTest {
     private SuggestionEngine suggestionEngine = new SuggestionEngine();
-
-    @Mock
-    private SuggestionsDatabase mockSuggestionDB;
-    private boolean testInstanceSame = false;
 
     // a test case given to us; tests to see if it can find hello if a character is incorrect
     @Test
     public void testGenerateSuggestionsIncorrect() throws Exception {
         suggestionEngine.loadDictionaryData( Paths.get( ClassLoader.getSystemResource("words.txt").getPath()));
-
-//        Assertions.assertTrue(testInstanceSame);
         Assertions.assertTrue(suggestionEngine.generateSuggestions("hellw").contains("hello"));
     }
 
@@ -33,8 +26,6 @@ public class SuggestionEngineTest {
     @Test
     public void testGenerateSuggestionsMissing() throws Exception {
         suggestionEngine.loadDictionaryData( Paths.get( ClassLoader.getSystemResource("words.txt").getPath()));
-
-//        Assertions.assertTrue(testInstanceSame);
         Assertions.assertTrue(suggestionEngine.generateSuggestions("ello").contains("hello"));
     }
 
@@ -42,8 +33,6 @@ public class SuggestionEngineTest {
     @Test
     public void testGenerateSuggestionsAdditional() throws Exception {
         suggestionEngine.loadDictionaryData( Paths.get( ClassLoader.getSystemResource("words.txt").getPath()));
-
-//        Assertions.assertTrue(testInstanceSame);
         Assertions.assertTrue(suggestionEngine.generateSuggestions("hellow").contains("hello"));
     }
 
@@ -51,8 +40,6 @@ public class SuggestionEngineTest {
     @Test
     public void testGenerateSuggestionsLessWithMistake() throws Exception {
         suggestionEngine.loadDictionaryData( Paths.get( ClassLoader.getSystemResource("words.txt").getPath()));
-
-//        Assertions.assertTrue(testInstanceSame);
         Assertions.assertTrue(suggestionEngine.generateSuggestions("hilo").contains("hello"));
     }
 
@@ -60,35 +47,18 @@ public class SuggestionEngineTest {
     @Test
     public void testGenerateSuggestionsMoreWithMistake() throws Exception {
         suggestionEngine.loadDictionaryData( Paths.get( ClassLoader.getSystemResource("words.txt").getPath()));
-
-//        Assertions.assertTrue(testInstanceSame);
         Assertions.assertTrue(suggestionEngine.generateSuggestions("hellaw").contains("hello"));
     }
 
     // a test case given to us; returns blank if the word matches with one in the database
     @Test
-    public void testGenerateSuggestionsFail() throws Exception {
+    public void testGenerateSuggestionWordIsCorrect() throws Exception {
         suggestionEngine.loadDictionaryData( Paths.get( ClassLoader.getSystemResource("words.txt").getPath()));
-
-        testInstanceSame = true;
+        boolean testInstanceSame = false;
+        String result = suggestionEngine.generateSuggestions("hello");
+        if(result.equals("")) { testInstanceSame = true; }
         Assertions.assertTrue(testInstanceSame);
         Assertions.assertFalse(suggestionEngine.generateSuggestions("hello").contains("hello"));
     }
 
-    // a test case given to us; uses mockito and tests to see if the word "test" can be generated with
-    // a word with one character less if it will return blank if the string matches
-    @Test
-    public void testSuggestionsAsMock() {
-        Map<String,Integer> wordMapForTest = new HashMap<>();
-
-        wordMapForTest.put("test", 1);
-
-        Mockito.when(mockSuggestionDB.getWordMap()).thenReturn(wordMapForTest);
-
-        suggestionEngine.setWordSuggestionDB(mockSuggestionDB);
-
-        Assertions.assertFalse(suggestionEngine.generateSuggestions("test").contains("test"));
-
-        Assertions.assertTrue(suggestionEngine.generateSuggestions("tes").contains("test"));
-    }
 }
